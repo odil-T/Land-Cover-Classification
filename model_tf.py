197742da3e4cd1673a123d01da26ef1fbbeda1e5
Odilbek Tokhirov <142138394+odil-T@users.noreply.github.com>
2024-05-05 17:57:48 +0500

    Delete model_tf.py

--- model_tf.py
+++
@@ -1,8 +0,0 @@
-model = build_unet(image_patch_size, len(color_mapping))
-
-model.compile(loss="sparse_categorical_crossentropy",
-              optimizer="adam",
-              metrics=["accuracy"])
-model.fit(X_train, y_train, validation_data=[X_valid, y_valid], epochs=100)
-
-model.save("models/model_sem_seg_dubai.h5")
