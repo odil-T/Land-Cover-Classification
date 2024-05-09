64ed14aacbaf92276cd0a5944af2646f8f674327
Odilbek Tokhirov <142138394+odil-T@users.noreply.github.com>
2024-05-09 13:38:05 +0500

    Add files via upload

--- semantic-segmentation/infer_and_display.py
+++ semantic-segmentation/infer_and_display.py
@@ -1,15 +1,27 @@
-import numpy as np
-import torch
+"""
+Displays original images from the validation set along with their ground truth masks and predicted masks for comparison.
+The predicted masks are produced by a pretrained model.
+The samples are taken randomly.
+You may specify the number of samples to display.
+"""
+
+import os
+import re
 import cv2
+import dotenv
+import random
+import numpy as np
+from PIL import Image
+from unet_torch import *
 from matplotlib import pyplot as plt
 from torchvision.transforms import ToTensor
-from unet_torch import *
-from PIL import Image
-import os
-import dotenv
 
 
 dotenv.load_dotenv()
+
+# Specify number of samples to display from the validation set
+num_samples = 5
+
 colormap = [
     (0, 0, 0),        # Black for the first class
     (255, 0, 0),      # Red
@@ -21,6 +33,8 @@ colormap = [
     (0, 255, 255),    # Cyan
     (255, 192, 203)   # Pink
 ]
+num_classes = int(os.getenv("NUM_CLASSES"))
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def resize_and_pad(image, target_size, is_mask):
@@ -56,73 +70,83 @@ def resize_and_pad(image, target_size, is_mask):
     return padded_image
 
 
-def display_results(model, target_size, image_path):
+def display_results(n, model):
     """
-    Displays an image, its ground truth mask, and its prediction mask based on the provided image path.
+    Displays random N number of images, ground truth masks, and predicted masks from the validation set of the
+    OpenEarthMap dataset.
 
     Args:
-        model:
-        target_size:
-        image_path:
+        n (int): The number of random samples from the validation set to display.
+        model: PyTorch model to use for semantic segmentation.
     """
 
-    image = Image.open(image_path)
-    image = np.array(image)
-    image_original = resize_and_pad(image, target_size, False)
-    image = ToTensor()(image_original).to(device)
-    image = image.unsqueeze(0)
 
-    true_mask = Image.open(image_path.replace("images", "labels"))
-    true_mask = np.array(true_mask)
+    height = int(os.getenv("TARGET_HEIGHT"))
+    width = int(os.getenv("TARGET_WIDTH"))
 
-    with torch.no_grad():
-        pred = model(image)  # torch.Tensor (1, 9, 1024, 1024)
+    root_data_dir = "data/OpenEarthMap/OpenEarthMap_wo_xBD"
+    filenames_file = "val_wo_xBD.txt"
 
-    pred = nn.Softmax(dim=1)(pred)
-    pred = pred.cpu()
-    pred = np.argmax(pred, axis=1)
-    pred = pred.squeeze(0)
+    with open(os.path.join(root_data_dir, filenames_file), "r") as f:
+        filenames = [re.sub(r'\n+$', '', line) for line in f.readlines()]  # ["aachen_1.tif", "aachen_10.tif", ...]
 
-    # true mask
-    true_mask_output = np.zeros((true_mask.shape[0], true_mask.shape[1], 3), dtype=np.uint8)
-    for class_id, color in enumerate(colormap):
-        true_mask_output[true_mask == class_id] = color
+    random_n_filenames = random.sample(filenames, n)
 
-    # predicted mask
-    pred_mask_output = np.zeros((pred.shape[0], pred.shape[1], 3), dtype=np.uint8)
-    for class_id, color in enumerate(colormap):
-        pred_mask_output[pred == class_id] = color
+    for filename in random_n_filenames:
+        location_dir = str(re.search(r'^(.*)_(?=\d)', filename).group(1))  # for e.g. "aachen"
+
+        image_path = os.path.join(root_data_dir, location_dir, "images", filename)
+        mask_path = os.path.join(root_data_dir, location_dir, "labels", filename)
+
+        image = Image.open(image_path)
+        image = np.array(image)
+        image_original = resize_and_pad(image, (height, width), False)
+        image = ToTensor()(image_original).to(device)
+        image = image.unsqueeze(0)
+
+        true_mask = Image.open(mask_path)
+        true_mask = np.array(true_mask)
+
+        with torch.no_grad():
+            pred = model(image)  # torch.Tensor (1, 9, 1024, 1024)
+
+        pred = nn.Softmax(dim=1)(pred)
+        pred = pred.cpu()
+        pred = np.argmax(pred, axis=1)
+        pred = pred.squeeze(0)
+
+        # True mask
+        true_mask_output = np.zeros((true_mask.shape[0], true_mask.shape[1], 3), dtype=np.uint8)
+        for class_id, color in enumerate(colormap):
+            true_mask_output[true_mask == class_id] = color
+
+        # Predicted mask
+        pred_mask_output = np.zeros((pred.shape[0], pred.shape[1], 3), dtype=np.uint8)
+        for class_id, color in enumerate(colormap):
+            pred_mask_output[pred == class_id] = color
 
 
-    fig, axs = plt.subplots(1, 3, figsize=(10, 5))
+        # Display images
+        fig, axs = plt.subplots(1, 3, figsize=(10, 5))
 
-    axs[0].imshow(image_original, cmap='gray')
-    axs[0].axis('off')
-    axs[0].set_title("Original Image")
+        axs[0].imshow(image_original, cmap='gray')
+        axs[0].axis('off')
+        axs[0].set_title("Original Image")
 
-    axs[1].imshow(true_mask_output)
-    axs[1].axis('off')
-    axs[1].set_title("Ground Truth Mask")
+        axs[1].imshow(true_mask_output)
+        axs[1].axis('off')
+        axs[1].set_title("Ground Truth Mask")
 
-    axs[2].imshow(pred_mask_output)
-    axs[2].axis('off')
-    axs[2].set_title("Predicted Mask")
+        axs[2].imshow(pred_mask_output)
+        axs[2].axis('off')
+        axs[2].set_title("Predicted Mask")
 
-    plt.show()
+        plt.show()
 
 
-height = int(os.getenv("TARGET_HEIGHT"))
-width = int(os.getenv("TARGET_WIDTH"))
-num_classes = int(os.getenv("NUM_CLASSES"))
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-image_path = "data/OpenEarthMap/OpenEarthMap_wo_xBD/viru/images/viru_47.tif"
-
-# Load Model
-# model = torch.load("models/unet_sem_seg_2024-05-05--13-07-27/unet_sem_seg.pth")  # old model
+# Load pretrained model
 model = UNet(3, num_classes).to(device)
 checkpoint = torch.load("models/unet_sem_seg_2024-05-07--05-45-34/unet_sem_seg_checkpoint_epoch40.pt")
-model.load_state_dict(checkpoint["model_state_dict"])  # new model
+model.load_state_dict(checkpoint["model_state_dict"])
 
-
-display_results(model, (height, width), image_path)
+display_results(num_samples, model)
