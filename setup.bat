906023a920abb061e4603cb9cb952bde82820681
Odilbek Tokhirov <142138394+odil-T@users.noreply.github.com>
2024-06-14 16:29:56 +0500

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,10 @@
+conda env create -f environment.yml
+
+mkdir best_models
+cd best_models
+
+curl -o "segformer_sem_seg_checkpoint_epoch35.pt" "https://huggingface.co/odil111/segformer-fine-tuned-on-openearthmap/resolve/main/segformer_sem_seg_2024-06-05--16-54-31/segformer_sem_seg_checkpoint_epoch35.pt?download=true"
+
+curl -o "best.pt" "https://huggingface.co/odil111/yolov8m-seg-fine-tuned-on-spacenetv2/resolve/main/yolov8m_inst_seg_2024-06-11--15-57-15/weights/best.pt?download=true"
+
+conda init
